d4cba6f3d22006dc9eaab166a602746a73f59bff
satthurikranthi <satthurikranthi@gmail.com>
2024-11-12 13:04:31 +0530

    Add files via upload

---
+++ intro/app.js
@@ -0,0 +1,11 @@
+console.log("this message is executing thru javascript Engine");
+
+
+for(let i=1;i<=100;i++){
+    console.log(i);
+}
+
+
+let myHeading = document.createElement("h1");
+myHeading.innerHTML = "This is created using browsers javascriptengine.";
+document.body.append(myHeading);
